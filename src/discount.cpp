159ab5add3c35e78e7aebe34e0dd039b441a1135
Guillaume Horel <guillaume.horel@serenitascapital.com>
2016-10-03 18:00:53 -0400

    enable extrapolation

--- src/discount.cpp
+++ src/discount.cpp
@@ -84,6 +84,7 @@ Rcpp::List discountCurveEngine(Rcpp::List rparams,
         }
         auto ts = getTermStructure(interpWhat, interpHow, settlementDate, curveInput, termStructureDayCounter, tolerance);
         curve = ts;
+        curve->enableExtrapolation();
     }
 
     // Return discount, forward rate, and zero coupon curves
