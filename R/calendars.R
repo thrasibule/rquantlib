3ecce5d066f78a79c7a1070f4128087070464554
Guillaume Horel <guillaume.horel@serenitascapital.com>
2016-10-18 17:29:11 -0400

    expose more bindings of the yts

--- R/calendars.R
+++ R/calendars.R
@@ -22,7 +22,9 @@
 Calendar <- R6Class("Calendar",
                     public = list(
                         initialize = function(name) {
-                            private$ptr <- getCalendar(name)
+                            if(!missing(name)) {
+                                private$ptr <- getCalendar(name)
+                            }
                         },
                         isBusinessDay = function(dates) {
                             isBusinessDay(private$ptr, dates)
